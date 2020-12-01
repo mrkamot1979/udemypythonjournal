16231dc4334dc5af67caf5bee266fbf7388c281c
nino <nino_roldan@yahoo.com>
2020-12-01 18:44:38 +0900

    upload

---
+++ app.py
@@ -0,0 +1,27 @@
+menu = """Please select one of the following options:
+1) Add new entry for today.
+2) View entries.
+3) Exit.
+
+your selection : """
+
+welcome = "Welcome to the programming diary!"
+
+print(welcome)
+
+while (user_input := input(menu))!= "3": #walrus feature available in 3.8
+    if (user_input == "1"):
+        print("Adding")
+    elif (user_input == "2"):
+        print("Viewing Entries")
+    else:
+        print("Invalid option, please try again")
+
+
+
+
+        
+
+
+
+
