c3d2a49c6fadcbbe860ab703c6ed5ff57e8715b6
nino <nino_roldan@yahoo.com>
2020-12-03 13:48:46 +0900

    creating the database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+entries = []
+
+def add_entry():
+    entry_content = input("What have you learned today?")
+    entry_date = input ("Enter the date: ")
+
+    {"content" : entry_content, "date" : entry_date}
+
+dev view_entry():
+    pass 
