bcb9371901936c8f2d730c0b35fb20192f92e92d
nino <nino_roldan@yahoo.com>
2020-12-03 22:45:18 +0900

    Done refactoring

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from database import add_entry, view_entry
-
+from database import add_entry, get_entries
 
 menu = """Please select one of the following options:
 1) Add new entry for today.
@@ -10,15 +9,23 @@ your selection : """
 
 welcome = "Welcome to the programming diary!"
 
+def prompt_new_entry():
+    entry_content = input("What have you learned today?: ")
+    entry_date = input ("Enter the date: ")
+    add_entry(entry_content, entry_date)
+
+def view_entries(entries):
+    for entry in entries:
+            print(f"{entry['content']}\n{entry['date']}\n\n")
+
+
 print(welcome)
 
 while (user_input := input(menu))!= "3": #walrus feature available in 3.8, essentially it becomes a BOOLEAN
     if (user_input == "1"):
-        entry_content = input("What have you learned today?: ")
-        entry_date = input ("Enter the date: ")
-        add_entry(entry_content, entry_date)
+        prompt_new_entry()
     elif (user_input == "2"):
-        view_entry()
+        view_entries(get_entries())       
     else:
         print("Invalid option, please try again")
 
--- database.py
+++ database.py
@@ -3,8 +3,7 @@ entries = []
 def add_entry(entry_content, entry_date):
     entries.append({"content" : entry_content, "date" : entry_date})
 
-def view_entry():
-    for entry in entries:
-        print(f"{entry['content']}\n{entry['date']}\n\n")
+def get_entries():
+   return entries
         
 
