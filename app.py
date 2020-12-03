4703401fc55554960ef55e8f21dbb93cfefa4a1a
nino <nino_roldan@yahoo.com>
2020-12-03 14:58:28 +0900

    completed the features

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+from database import add_entry, view_entry
+
+
 menu = """Please select one of the following options:
 1) Add new entry for today.
 2) View entries.
@@ -11,9 +14,9 @@ print(welcome)
 
 while (user_input := input(menu))!= "3": #walrus feature available in 3.8, essentially it becomes a BOOLEAN
     if (user_input == "1"):
-        print("Adding")
+        add_entry()
     elif (user_input == "2"):
-        print("Viewing Entries")
+        view_entry()
     else:
         print("Invalid option, please try again")
 
--- database.py
+++ database.py
@@ -1,10 +1,15 @@
 entries = []
 
 def add_entry():
-    entry_content = input("What have you learned today?")
+    entry_content = input("What have you learned today? ")
     entry_date = input ("Enter the date: ")
 
-    {"content" : entry_content, "date" : entry_date}
+    entries.append({"content" : entry_content, "date" : entry_date})
 
-dev view_entry():
-    pass 
+def view_entry():
+    for entry in entries:
+        print(f"{entry['date']}\n{entry['content']}\n\n")
+        
+
+add_entry()
+view_entry()
